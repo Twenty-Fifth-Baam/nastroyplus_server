1d4a8f9ffad11e16896f1a1143951f2b63cb281b
Maksim Khvorostov <maksimm.khvorostov@mail.com>
2022-05-22 15:05:27 +0300

    new fix

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: "postgres",
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-    }
-);
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: "postgres",
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//     }
+// );
 
-// module.exports = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   protocol: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
