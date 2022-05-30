0a39859925cceb9a9a04908790031e8f09107f7c
Maksim Khvorostov <maksimm.khvorostov@mail.com>
2022-05-30 20:31:38 +0300

    fix

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,111 +1,122 @@
 const userService = require("../service/user-service");
-const { validationResult } = require("express-validator");
+const {validationResult} = require("express-validator");
 const ApiError = require("../exceptions/api-error");
 const tokenService = require("../service/token-service");
 
 class UserController {
-  async registration(req, res, next) {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return next(
-          ApiError.BadRequest("Ошибка при валидации", errors.array())
-        );
-      }
-      const { email, password } = req.body;
-      const userData = await userService.registration(email, password);
-      res.cookie("refreshToken", userData.refreshToken, {
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        sameSite: "none",
-        secure:true
-      });
-      return res.json(userData);
-    } catch (e) {
-      next(e);
+    async registration(req, res, next) {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return next(
+                    ApiError.BadRequest("Ошибка при валидации", errors.array())
+                );
+            }
+            const {email, password} = req.body;
+            const userData = await userService.registration(email, password);
+            res.cookie("refreshToken", userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+                sameSite: "none",
+                secure: true
+            });
+            return res.json(userData);
+        } catch (e) {
+            next(e);
+        }
     }
-  }
-  async login(req, res, next) {
-    try {
-      const { email, password } = req.body;
-      const userData = await userService.login(email, password);
-      res.cookie("refreshToken", userData.refreshToken, {
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        sameSite:'none',
-        secure:true
-      });
-      return res.json(userData);
-    } catch (e) {
-      next(e);
-    }
-  }
-  async logout(req, res, next) {
-    try {
-      const { refreshToken } = req.cookies;
-      const token = await userService.logout(refreshToken);
-      res.clearCookie("refreshToken");
-      return res.json(token);
-    } catch (e) {
-      next(e);
-    }
-  }
-  async activate(req, res, next) {
-    console.log("jhhusfdgbhzjhsfgr");
-    try {
-      const activationLink = req.params.link;
-      await userService.activate(activationLink);
-      return res.redirect(process.env.CLIENT_URL);
-    } catch (e) {
-      next(e);
-    }
-  }
-  async refresh(req, res, next) {
-    try {
-      const { refreshToken } = req.cookies;
-      const userData = await userService.refresh(refreshToken);
-      res.cookie("refreshToken", userData.refreshToken, {
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-      });
-      return res.json(userData);
-    } catch (e) {
-      next(e);
-    }
-  }
-  async isauth(req, res, next) {
-    try {
-      const authorizationHeader = req.headers.authorization;
-      if (!authorizationHeader) {
-        return next(ApiError.UnauthorizedError());
-      }
 
-      const accessToken = authorizationHeader.split(" ")[1];
-      if (!accessToken) {
-        return next(ApiError.UnauthorizedError());
-      }
+    async login(req, res, next) {
+        try {
+            const {email, password} = req.body;
+            const userData = await userService.login(email, password);
+            res.cookie("refreshToken", userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+                sameSite: 'none',
+                secure: true
+            });
+            return res.json(userData);
+        } catch (e) {
+            next(e);
+        }
+    }
 
-      const userData = tokenService.validateAccessToken(accessToken);
-      if (!userData) {
-        return next(ApiError.UnauthorizedError());
-      }
-      return res.json({
-        email: userData.email,
-        id: userData.id,
-        isActivated: userData.isActivated,
-      });
-    } catch (e) {
-      next(e);
+    async logout(req, res, next) {
+        try {
+            const {refreshToken} = req.cookies;
+            const token = await userService.logout(refreshToken);
+            res.clearCookie("refreshToken", {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+                sameSite: 'none',
+                secure: true
+            });
+            return res.json(token);
+        } catch (e) {
+            next(e);
+        }
     }
-  }
-  async getUsers(req, res, next) {
-    try {
-      const users = await userService.getAllUsers();
-      return res.json(users);
-    } catch (e) {
-      next(e);
+
+    async activate(req, res, next) {
+        console.log("jhhusfdgbhzjhsfgr");
+        try {
+            const activationLink = req.params.link;
+            await userService.activate(activationLink);
+            return res.redirect(process.env.CLIENT_URL);
+        } catch (e) {
+            next(e);
+        }
     }
-  }
+
+    async refresh(req, res, next) {
+        try {
+            const {refreshToken} = req.cookies;
+            const userData = await userService.refresh(refreshToken);
+            res.cookie("refreshToken", userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+            });
+            return res.json(userData);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    async isauth(req, res, next) {
+        try {
+            const authorizationHeader = req.headers.authorization;
+            if (!authorizationHeader) {
+                return next(ApiError.UnauthorizedError());
+            }
+
+            const accessToken = authorizationHeader.split(" ")[1];
+            if (!accessToken) {
+                return next(ApiError.UnauthorizedError());
+            }
+
+            const userData = tokenService.validateAccessToken(accessToken);
+            if (!userData) {
+                return next(ApiError.UnauthorizedError());
+            }
+            return res.json({
+                email: userData.email,
+                id: userData.id,
+                isActivated: userData.isActivated,
+            });
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    async getUsers(req, res, next) {
+        try {
+            const users = await userService.getAllUsers();
+            return res.json(users);
+        } catch (e) {
+            next(e);
+        }
+    }
 }
 
 module.exports = new UserController();
